883100231dc030ef23f1b1d5a1934ff2227a2fc3
Sol <solmseda@gmail.com>
2023-08-29 11:55:42 -0300

    Bugfix on Criar contato
    The world 'formulario', was wrong on exemplo/MeuSite/contatos/view.py, making it impossible to create a new contact.

--- exemplo/MeuSite/contatos/views.py
+++ exemplo/MeuSite/contatos/views.py
@@ -5,7 +5,6 @@ from contatos.forms import ContatoModel2Form
 from django.http.response import HttpResponseRedirect
 from django.urls.base import reverse_lazy
 
-# Create your views here.
 class ContatoListView(View):
     def get(self, request, *args, **kwargs):
         pessoas = Pessoa.objects.all()
@@ -14,12 +13,12 @@ class ContatoListView(View):
 
 class ContatoCreateView(View):
     def get(self, request, *args, **kwargs):
-        contexto = {'formulario': ContatoModel2Form,}
+        contexto = {'formulario': ContatoModel2Form, }
         return render(request, "contatos/criaContato.html", contexto)
 
     def post(self, request, *args, **kwargs):
         formulario = ContatoModel2Form(request.POST)
-        if fomulario.is_valid():
+        if formulario.is_valid():
             contato = formulario.save()
             contato.save()
             return HttpResponseRedirect(reverse_lazy("contatos:lista-contatos"))
